9eeb26feefa0c3cbcb2e405462b513f1e17d14cc
luzhisheng <aiyingfeng110@qq.com>
2022-12-06 14:40:20 +0800

    启动文件

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from spider.京东搜索列表页面爬虫 import 京东搜索列表页面爬虫
+from spider.京东商品评论爬虫 import 京东商品评论爬虫
+from tool.创建京东商品评论爬虫任务 import 创建京东商品评论爬虫任务
 
 
-# Press the green button in the gutter to run the script.
+def jop():
+    a = 京东搜索列表页面爬虫()
+    a.run()
+
+    d = 创建京东商品评论爬虫任务()
+    list_dict = []
+    task_id = 'dzx-110000'
+    sql = f"SELECT project_id, keyword, url FROM jd_search_keyword where project_id = '{task_id}'"
+    res = d.eb_supports.query(sql)
+    for project_id, keyword, url in res:
+        item = {"project_id": project_id, "keyword": keyword, "url": url}
+        list_dict.append(item)
+    d.project(list_dict)
+
+    b = 京东商品评论爬虫()
+    b.run()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    jop()
