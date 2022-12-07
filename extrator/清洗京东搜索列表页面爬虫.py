456d04ff0d746944a607f0c830c2bb828a144809
luzhisheng <aiyingfeng110@qq.com>
2022-12-07 15:18:12 +0800

    增加 清洗京东搜索列表页面爬虫 文件

--- extrator/清洗京东搜索列表页面爬虫.py
+++ extrator/清洗京东搜索列表页面爬虫.py
@@ -16,24 +16,32 @@ class 清洗京东搜索列表页面爬虫(Base):
             """
             self.log(f"执行的sql{sql}")
             res = self.eb_supports.query(sql)
+            if not res:
+                break
+
             for project_id, keyword, url, data in res:
                 selector = etree.HTML(data)
                 title = selector.xpath('//div[contains(@class,"p-name")]/a/@title')[0]
                 data_price = selector.xpath('//div[contains(@class,"p-price")]/strong/i/text()')[0]
                 goods_icons = selector.xpath('//div[contains(@class,"p-icons")]/i/text()')
-                shop_name = selector.xpath('//span[contains(@class,"J_im_icon")]/a/text()')[0]
+                shop_name = selector.xpath('//span[contains(@class,"J_im_icon")]/a/text()')
+
+                if shop_name:
+                    shop_name = shop_name[0]
+                else:
+                    shop_name = ''
+
                 item = {
                     "project_id": project_id,
                     "keyword": keyword,
                     "url": url,
                     "title": title,
-                    "data-price": data_price,
-                    "goods-icons": goods_icons,
-                    "shop-name": shop_name
+                    "data_price": data_price,
+                    "goods_icons": "/".join(goods_icons),
+                    "shop_name": shop_name
                 }
                 list_res.append(item)
             db_res = self.eb_supports.insert_many('clean_jd_search_keyword', list_res)
-
             if db_res >= 0:
                 self.log(f"入库成功 {db_res}")
             offset += 200
