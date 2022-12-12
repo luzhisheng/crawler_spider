cd42bc59d4f4b181e4d7085fdf2acbbd590a7666
luzhisheng <aiyingfeng110@qq.com>
2022-12-12 16:08:53 +0800

    学习 pyecharts Flask demo

--- web/server.py
+++ web/server.py
@@ -1,32 +1,30 @@
-from flask import Flask
-from jinja2 import Markup, Environment, FileSystemLoader
-from pyecharts.globals import CurrentConfig
-
-# 关于 CurrentConfig，可参考 [基本使用-全局变量]
-CurrentConfig.GLOBAL_ENV = Environment(loader=FileSystemLoader("./templates"))
-
+from random import randrange
+from flask import Flask, render_template
 from pyecharts import options as opts
 from pyecharts.charts import Bar
-
 
 app = Flask(__name__, static_folder="templates")
 
 
 def bar_base() -> Bar:
-    c = (
-        Bar()
-            .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
-            .add_yaxis("商家A", [500, 20, 36, 10, 75, 90])
-            .add_yaxis("商家B", [15, 25, 16, 55, 48, 8])
-            .set_global_opts(title_opts=opts.TitleOpts(title="Bar-基本示例", subtitle="我是副标题"))
-    )
-    return c
+    bar = Bar()
+    bar.add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
+    bar.add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
+    bar.add_yaxis("商家A", [randrange(0, 100) for _ in range(6)])
+    bar.add_yaxis("商家B", [randrange(0, 100) for _ in range(6)])
+    bar.set_global_opts(title_opts=opts.TitleOpts(title="Bar-基本示例", subtitle="我是副标题"))
+    return bar
 
 
 @app.route("/")
 def index():
+    return render_template("index.html")
+
+
+@app.route("/barChart")
+def get_bar_chart():
     c = bar_base()
-    return Markup(c.render_embed())
+    return c.dump_options_with_quotes()
 
 
 if __name__ == "__main__":
